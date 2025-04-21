db5faf3c5e9cb1a50b932f109993ab98d9d536fa
IsharWeb <isharwebdeveloper@gmail.com>
2025-04-21 10:10:40 +0500

    i connect fontend to backend

--- basic/backend/express.js
+++ basic/backend/express.js
@@ -1,18 +1,25 @@
-import  express  from "express";
+import express from "express";
+import { fileURLToPath } from "url"
+import path from "path"
 import dotenv from "dotenv"
 
 dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 // console.log(`app = ${app}`);
 
 app.get("/", (req, res) => {
-    res.send('<h1>Home Url Hellow Backend Js <h1/>')
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+    //res.send('<h1>Home Url Hellow Backend Js <h1/>')
 })
 
-app.get("/port", (req, res) => {app
+app.get("/port", (req, res) => {
     app
-    res.send('<h1>Port Url Hellow Backend Js <h1/>')
+
+    //res.send('<h1>Port Url Hellow Backend Js <h1/>')
 })
 
 
