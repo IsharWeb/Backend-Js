433f5ab38d4b46ca8e534b87ebf11b967b227b4e
IsharWeb <isharwebdeveloper@gmail.com>
2025-05-13 08:09:55 +0500

    i solve db connectionn error

--- fullstackWeb/backendWeb/src/db/db.js
+++ fullstackWeb/backendWeb/src/db/db.js
@@ -1,35 +1,35 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
+const connectDB = async () => {
+    try {
+        const connectDbHost = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`,
+            // {
+            //     useNewUrlParser: true,
+            //     useUnifiedTopology: true
+            // }
+        );
+
+        console.log(`\n✅ MongoDB connected!! On Host: ${connectDbHost.connection.host}`);
+    } catch (error) {
+        console.log("❌ DB Connection Error =", error);
+        process.exit(1);
+    }
+};
+
+// let url = 'mongodb+srv://isharwebdeveloper:isharkhan601@cluster0.causgax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 // const connectDB = async () => {
 
 //     try {
-//        const connectDbHost = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//        console.log("connectDbHost data = ", connectDbHost);
-
-//         console.log(`\n Mongodb was connected !! On Hoset ${connectDbHost.connect.host}`);
-
-
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         console.log('Databaise connecte sucessfuly');
 //     } catch (error) {
-//         console.log("DB Connecton Error = ", error);
-//         process.exit(1)
+//         console.log("Mongo DB connecting error ", error);
+//         process.exit(1);
 //     }
 
 // }
-
-let url = 'mongodb+srv://isharwebdeveloper:isharkhan601@cluster0.causgax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
-const connectDB = async () => {
-
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log('Databaise connecte sucessfuly');
-    } catch (error) {
-        console.log("Mongo DB connecting error ", error);
-        process.exit(1);
-    }
-
-}
 
 // const connectDB =  mongoose.connect(`${url}/${DB_NAME}`,  function(err, db) {
 //     if (err) {
